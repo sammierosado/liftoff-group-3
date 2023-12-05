7f26f0437be80e39f8c5ca024153c59b362031cb
ProoChuck <rssnow58@gmail.com>
2023-12-04 19:34:20 -0600

    Added spring boot/gradle

---
+++ src/main/java/brainyBunch/liftoffgroup3/LiftoffGroup3Application.java
@@ -0,0 +1,13 @@
+package brainyBunch.liftoffgroup3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftoffGroup3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftoffGroup3Application.class, args);
+	}
+
+}
---
+++ src/test/java/brainyBunch/liftoffgroup3/LiftoffGroup3ApplicationTests.java
@@ -0,0 +1,13 @@
+package brainyBunch.liftoffgroup3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftoffGroup3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
