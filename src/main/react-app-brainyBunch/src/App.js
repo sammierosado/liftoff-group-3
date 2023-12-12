f4bc803d874426eddeec3e287ede316caf8f4708
rebeccay12 <beccayeager97@gmail.com>
2023-12-11 19:59:32 -0600

    set up react within src/main

---
+++ src/main/react-app-brainyBunch/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Spotify Music App</h1>
+      <p>This is the homepage for the spotify music application.</p>
+    </div>
+  );
+}
+
+
+
+export default App;
