c117883bad629d70fa890020fa152f46a7bc9da8
rebeccay12 <beccayeager97@gmail.com>
2023-12-12 22:08:27 -0600

    added homepage and liked songs

--- src/main/react-app-brainyBunch/src/App.js
+++ src/main/react-app-brainyBunch/src/App.js
@@ -1,18 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
-
 import React from 'react';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import LikedSongs from "./components/LikedSongs";
+
+
 
 function App() {
   return (
-    <div>
-      <h1>Spotify Music App</h1>
-      <p>This is the homepage for the spotify music application.</p>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/likedsongs" element={<LikedSongs />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-
-
 export default App;
+
+
+
