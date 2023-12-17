721f878528d542262da857f5741b687e4b409290
ProoChuck <rssnow58@gmail.com>
2023-12-17 14:48:23 -0600

    Fixed an issue with the date display that would only show up to November for the month.  Also made some minor style changes in the styles.css.

--- src/main/react-app-brainyBunch/public/time-date.js
+++ src/main/react-app-brainyBunch/public/time-date.js
@@ -5,10 +5,10 @@ function timeDateToggle() {
     const minutes = currentTime.getMinutes();
     const seconds = currentTime.getSeconds();
 
+    const month = currentTime.getMonth();// Month (0-11, adjust for display)
     const date = currentTime.getDate(); // Day of the month (1-31)
-    const month = currentTime.getMonth() + 1; // Month (0-11, adjust for display)
     const year = currentTime.getFullYear(); // Year
-  
+
     // Format the time and date based on your desired format
     const formattedTime = `${hours}:${minutes}:${seconds} - ${date}/${month}/${year}`;
   
@@ -17,7 +17,13 @@ function timeDateToggle() {
     // Keep the time updated every second (optional)
     setInterval(() => {
       const currentTime = new Date();
-      const formattedTime = `Real Time:${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}/ Today's Date:${currentTime.getMonth()}-${currentTime.getDate()}-${currentTime.getFullYear()}`;
+      let month = currentTime.getMonth() + 1; // Get the month with an offset of 1
+
+      if (month === 13) { // Check if current month is December
+        month = 1; // Set month to 1 to represent January
+      }
+
+      const formattedTime = `Real Time:${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}/ Today's Date:${month}-${currentTime.getDate()}-${currentTime.getFullYear()}`;
       timeElement.textContent = formattedTime;
     }, 1000);
   
