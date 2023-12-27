c6f392583c859f47f19ebcda52f55ac379bfea52
colemankus <hanshi2929@gmail.com>
2023-12-27 12:55:06 -0800

    added pinterest style boards on home page

--- src/main/react-app-brainyBunch/src/components/HomePage.js
+++ src/main/react-app-brainyBunch/src/components/HomePage.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "../css/homePage.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import CollectionBoard from "./CollectionBoard";
 
 function HomePage() {
   return (
     <div className="homePage">
       <div className="nav">
-        <h1 className="heading">Welcome to my website</h1>
-        <span className="nav_routes" >
+        <span className="nav_routes">
           <Link to="/register">Register User</Link>&nbsp;&nbsp;
           <Link to="/login">Login</Link>
         </span>
       </div>
-      <p>Here's some content for the homepage</p>
+      <CollectionBoard />
     </div>
   );
 }
