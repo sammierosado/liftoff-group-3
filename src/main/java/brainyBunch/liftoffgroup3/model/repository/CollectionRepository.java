403824d724b9ba3cfd57c632d0e85585f839cfdc
rebeccay12 <beccayeager97@gmail.com>
2024-01-06 14:39:02 -0600

    Added collection to mySQL

---
+++ src/main/java/brainyBunch/liftoffgroup3/model/repository/CollectionRepository.java
@@ -0,0 +1,13 @@
+package brainyBunch.liftoffgroup3.model.repository;
+import brainyBunch.liftoffgroup3.model.Collection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import brainyBunch.liftoffgroup3.model.User;
+import brainyBunch.liftoffgroup3.model.UserProfileDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+@Repository
+public interface CollectionRepository extends JpaRepository<Collection, Long> {
+    // You can define custom queries or methods here if needed
+}
