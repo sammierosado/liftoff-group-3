c94efd7de2961274d2659589958629b25572a5f2
rebeccay12 <beccayeager97@gmail.com>
2024-01-11 16:03:48 -0600

    update LikedSongsController

--- src/main/react-app-brainyBunch/src/components/RandomMusicPage.js
+++ src/main/react-app-brainyBunch/src/components/RandomMusicPage.js
@@ -49,34 +49,37 @@ function RandomMusicPage() {
 
   const handleLike = async (index) => {
     const updatedAlbums = [...albums];
-    updatedAlbums[index].liked = !updatedAlbums[index].liked;
+    const likedAlbum = updatedAlbums[index];
+    likedAlbum.liked = !likedAlbum.liked;
     setAlbums(updatedAlbums);
-
-    if (updatedAlbums[index].liked) {
-        setLikedAlbums([...likedAlbums, updatedAlbums[index]]);
-
-        // Send a request to the backend to save the liked album
-        const { id, name, artists } = updatedAlbums[index];
-        const likedSong = {
-            id,
-            albumName: name,
-            artistName: artists[0].name,
-        };
-
+  
+    if (likedAlbum.liked) {
+      setLikedAlbums((prevLikedAlbums) => [...prevLikedAlbums, likedAlbum]);
+  
+      // Pull the song title and artist information if tracks is defined
+      if (likedAlbum.tracks) {
+        const { id, name, tracks } = likedAlbum;
+        const likedSongs = tracks.map((track) => ({
+          albumId: id,
+          albumName: name,
+          artistName: track.artists[0].name,
+          songTitle: track.name,
+        }));
+  
+        // Send a request to the backend to save the liked songs
         await fetch("http://localhost:8080/api/liked-songs", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(likedSong),
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(likedSongs),
         });
+      }
     } else {
-        const filteredLikedAlbums = likedAlbums.filter(
-            (album) => album.id !== updatedAlbums[index].id
-        );
-        setLikedAlbums(filteredLikedAlbums);
+      // Can add unlike if wanted later
     }
   };
+  
 
 
 
@@ -139,74 +142,3 @@ function RandomMusicPage() {
 }
 
 export default RandomMusicPage;
-
-
-
-// import { useEffect, useState } from "react";
-// import "../css/randomPage.css";
-
-// const CLIENT_ID = "2c8d20f72c914fe79dfd499fb8f9644e";
-// const CLIENT_SECRET = "9ba9d68e457a43aea82a41d0114e9aa8";
-
-// function RandomMusicPage() {
-//   const [accessToken, setAcessToken] = useState("");
-//   const [albums, setAlbums] = useState([]);
-
-//   useEffect(() => {
-//     // API Acess Token
-//     var authParameters = {
-//       method: "POST",
-//       headers: {
-//         "content-type": "application/x-www-form-urlencoded",
-//       },
-//       body:
-//         "grant_type=client_credentials&client_id=" +
-//         CLIENT_ID +
-//         "&client_secret=" +
-//         CLIENT_SECRET,
-//     };
-//     fetch("https://accounts.spotify.com/api/token", authParameters)
-//       .then((result) => result.json())
-//       .then((data) => setAcessToken(data.access_token));
-//   }, []);
-
-//   async function search() {
-//     var newAlbums = await fetch(
-//       "https://api.spotify.com/v1/browse/new-releases",
-//       {
-//         headers: {
-//           Authorization: "Bearer " + accessToken,
-//         },
-//       }
-//     )
-//       .then((response) => response.json())
-//       .then((data) => setAlbums(data.albums.items));
-//   }
-
-//   return (
-//     <div className="RandomMusicPage">
-//       <div>
-//         <button className="RandomButton" type="button" onClick={search}>
-//           Search for popular new albums!
-//         </button>
-//       </div>
-//       <div>
-//         {albums.map((album, i) => {
-//           return (
-//             <div className="RandomCard" key={i}>
-//               <img src={album.images[0].url} alt="alt text" />
-//               <div className="container">
-//                 <h4>
-//                   <b>{album.name}</b>
-//                 </h4>
-//                 <p>{album.artists[0].name}</p>
-//               </div>
-//             </div>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default RandomMusicPage;
