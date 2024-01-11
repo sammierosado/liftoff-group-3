a77d91a20b1a8d4ac3bfa588aeb0b52fc954eb56
colemankus <hanshi2929@gmail.com>
2024-01-11 14:22:16 -0600

    added ability to view randompage tracks as grid

--- src/main/react-app-brainyBunch/src/components/RandomMusicPage.js
+++ src/main/react-app-brainyBunch/src/components/RandomMusicPage.js
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import LikedSongsPage from "./LikedSongsPage";
 import CollectionPage from "./CollectionPage"; // Import CollectionPage
 import "../css/randomPage.css";
+import "../css/girdLayout.css";
 
 const CLIENT_ID = "2c8d20f72c914fe79dfd499fb8f9644e";
 const CLIENT_SECRET = "9ba9d68e457a43aea82a41d0114e9aa8";
@@ -12,6 +13,7 @@ function RandomMusicPage() {
   const [albums, setAlbums] = useState([]);
   const [likedAlbums, setLikedAlbums] = useState([]);
   const [selectedAlbum, setSelectedAlbum] = useState(null);
+  const [styleSelected, setStyleSelected] = useState("column");
 
   useEffect(() => {
     var authParameters = {
@@ -73,46 +75,97 @@ function RandomMusicPage() {
 
   return (
     <div className="RandomMusicPage">
+      <form>
+        <label for="style">Choose a style:</label>
+        <select
+          id="style"
+          name="style"
+          onChange={(event) => setStyleSelected(event.target.value)}
+        >
+          <option value="column">Column</option>
+          <option value="grid">Grid</option>
+        </select>
+      </form>
       <div>
         <button className="RandomButton" type="button" onClick={search}>
           Search for popular new albums!
         </button>
       </div>
-      <div>
-        {albums.map((album, i) => (
-          <div className="RandomCard" key={i}>
-            <img
-              src={album.images[0].url}
-              alt="alt text"
-              className="randomImg"
-            />
-            <div className="container">
-              <h4>
-                <b>{album.name}</b>
-              </h4>
-              <p>{album.artists[0].name}</p>
-              <button onClick={() => handleLike(i)}>
-                {album.liked ? "Unlike" : "Like"}
-              </button>
-              <form>
-                <label for="userCollections">Choose a collection:</label>
-                <select name="collections" id="collections">
-                  <option value="test">Test</option>
-                  <option value="test">Test</option>
-                  <option value="test">Test</option>
-                  <option value="test">Test</option>
-                </select>
-                <br />
-                <input
-                  type="submit"
-                  value="Submit"
-                  onClick={() => handleAddToCollection()}
-                />
-              </form>
+      {styleSelected === "column" ? (
+        <div>
+          {albums.map((album, i) => (
+            <div className="RandomCard" key={i}>
+              <img
+                src={album.images[0].url}
+                alt="alt text"
+                className="randomImg"
+              />
+              <div>
+                <h4>
+                  <b>{album.name}</b>
+                </h4>
+                <p>{album.artists[0].name}</p>
+                <button onClick={() => handleLike(i)}>
+                  {album.liked ? "Unlike" : "Like"}
+                </button>
+                <form>
+                  <label for="userCollections">Choose a collection:</label>
+                  <select name="collections" id="collections">
+                    <option value="test">Test</option>
+                    <option value="test">Test</option>
+                    <option value="test">Test</option>
+                    <option value="test">Test</option>
+                  </select>
+                  <br />
+                  <input
+                    type="submit"
+                    value="Submit"
+                    onClick={() => handleAddToCollection()}
+                  />
+                </form>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ) : (
+        <div className="GridContainer">
+          {albums.map((album, i) => (
+            <div className="GridRandomCard" key={i}>
+              <div className="row">
+                <img
+                  src={album.images[0].url}
+                  alt="alt text"
+                  className="GridRandomImg"
+                />
+                <div>
+                  <h4>
+                    <b>{album.name}</b>
+                  </h4>
+                  <p>{album.artists[0].name}</p>
+                  <button onClick={() => handleLike(i)}>
+                    {album.liked ? "Unlike" : "Like"}
+                  </button>
+                  <form>
+                    <label for="userCollections">Choose a collection:</label>
+                    <select name="collections" id="collections">
+                      <option value="test">Test</option>
+                      <option value="test">Test</option>
+                      <option value="test">Test</option>
+                      <option value="test">Test</option>
+                    </select>
+                    <br />
+                    <input
+                      type="submit"
+                      value="Submit"
+                      onClick={() => handleAddToCollection()}
+                    />
+                  </form>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
       <div>
         {/* Uncomment this link when you have a LikedSongsPage component */}
         {/* <Link to="/likedsongs">Go to Liked Songs</Link> */}
@@ -133,72 +186,3 @@ function RandomMusicPage() {
 }
 
 export default RandomMusicPage;
-
-// import { useEffect, useState } from "react";
-// import "../css/randomPage.css";
-
-// const CLIENT_ID = "2c8d20f72c914fe79dfd499fb8f9644e";
-// const CLIENT_SECRET = "9ba9d68e457a43aea82a41d0114e9aa8";
-
-// function RandomMusicPage() {
-//   const [accessToken, setAcessToken] = useState("");
-//   const [albums, setAlbums] = useState([]);
-
-//   useEffect(() => {
-//     // API Acess Token
-//     var authParameters = {
-//       method: "POST",
-//       headers: {
-//         "content-type": "application/x-www-form-urlencoded",
-//       },
-//       body:
-//         "grant_type=client_credentials&client_id=" +
-//         CLIENT_ID +
-//         "&client_secret=" +
-//         CLIENT_SECRET,
-//     };
-//     fetch("https://accounts.spotify.com/api/token", authParameters)
-//       .then((result) => result.json())
-//       .then((data) => setAcessToken(data.access_token));
-//   }, []);
-
-//   async function search() {
-//     var newAlbums = await fetch(
-//       "https://api.spotify.com/v1/browse/new-releases",
-//       {
-//         headers: {
-//           Authorization: "Bearer " + accessToken,
-//         },
-//       }
-//     )
-//       .then((response) => response.json())
-//       .then((data) => setAlbums(data.albums.items));
-//   }
-
-//   return (
-//     <div className="RandomMusicPage">
-//       <div>
-//         <button className="RandomButton" type="button" onClick={search}>
-//           Search for popular new albums!
-//         </button>
-//       </div>
-//       <div>
-//         {albums.map((album, i) => {
-//           return (
-//             <div className="RandomCard" key={i}>
-//               <img src={album.images[0].url} alt="alt text" />
-//               <div className="container">
-//                 <h4>
-//                   <b>{album.name}</b>
-//                 </h4>
-//                 <p>{album.artists[0].name}</p>
-//               </div>
-//             </div>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default RandomMusicPage;
