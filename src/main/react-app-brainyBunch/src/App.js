f75a98d480a9dcc21f13ac8530b4b2b597678bea
varti2023 <varti.pant@gmail.com>
2024-01-11 22:14:45 -0600

    applying theme to all pages

--- src/main/react-app-brainyBunch/src/App.js
+++ src/main/react-app-brainyBunch/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
@@ -16,6 +16,18 @@ import StamplistPage from "./components/StamplistPage";
 import Setting from "./components/Setting";
 import ImagePage from "./components/ImagePage";
 function App() {
+  const darkMode = localStorage.getItem("spotify-mode");
+  const fontSize = localStorage.getItem("spotify-font-size");
+
+  useEffect(() => {
+    var bodyClasses =
+      (darkMode === "light" ? "" : "dark") +
+      " " +
+      (fontSize ? fontSize : "fsNormal");
+
+    document.body.className = bodyClasses;
+  });
+
   return (
     <Router>
       <div>
@@ -24,13 +36,13 @@ function App() {
           <Route path="/likedsongs" element={<LikedSongsPage />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />   
+          <Route path="/logout" element={<Logout />} />
           <Route path="/user" element={<UserProfile />} />
           <Route path="/randompage" element={<RandomMusicPage />} />
           <Route path="/collections" element={<CollectionPage />} />
-          <Route path="/stamplist" element={<StamplistPage/>} />
-          <Route path="/setting" element={<Setting/>} />
-          <Route path="/imagepage" element={<ImagePage/>} />
+          <Route path="/stamplist" element={<StamplistPage />} />
+          <Route path="/setting" element={<Setting />} />
+          <Route path="/imagepage" element={<ImagePage />} />
         </Routes>
       </div>
     </Router>
