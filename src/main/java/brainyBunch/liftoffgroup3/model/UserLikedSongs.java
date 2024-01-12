da4b3c8853ecbb31f82c58f1104413896c5bc049
rebeccay12 <beccayeager97@gmail.com>
2024-01-12 12:29:42 -0600

    userliked songs

--- src/main/java/brainyBunch/liftoffgroup3/model/UserLikedSongs.java
+++ src/main/java/brainyBunch/liftoffgroup3/model/UserLikedSongs.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
+@Entity
 public class UserLikedSongs {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
