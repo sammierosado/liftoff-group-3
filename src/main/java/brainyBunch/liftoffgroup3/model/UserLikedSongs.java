65e8456eaa47593f6228d0e87a182567b8e0bc00
rebeccay12 <beccayeager97@gmail.com>
2024-01-12 12:21:18 -0600

    update UserLikedSongs

--- src/main/java/brainyBunch/liftoffgroup3/model/UserLikedSongs.java
+++ src/main/java/brainyBunch/liftoffgroup3/model/UserLikedSongs.java
@@ -1,2 +1,65 @@
-package brainyBunch.liftoffgroup3.model;public class UserLikedSongs {
+package brainyBunch.liftoffgroup3.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+public class UserLikedSongs {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @NotBlank
+    public String username;
+
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastUpdatedDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getLastUpdatedDate() {
+        return lastUpdatedDate;
+    }
+
+    public void setLastUpdatedDate(LocalDateTime lastUpdatedDate) {
+        this.lastUpdatedDate = lastUpdatedDate;
+    }
+
+    public UserLikedSongs(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+    public UserLikedSongs() {
+    }
 }
