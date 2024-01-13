1489c4664a41de7d3a0391cefb45a2496d85a327
varti2023 <varti.pant@gmail.com>
2024-01-13 11:52:23 -0600

    added user likes into db

--- src/main/java/brainyBunch/liftoffgroup3/model/repository/LikedSongsRepository.java
+++ src/main/java/brainyBunch/liftoffgroup3/model/repository/LikedSongsRepository.java
@@ -3,7 +3,11 @@ import brainyBunch.liftoffgroup3.model.LikedSongs;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LikedSongsRepository extends JpaRepository<LikedSongs, Long> {
+
+    List<LikedSongs> findByUsername(String username);
 
 }
