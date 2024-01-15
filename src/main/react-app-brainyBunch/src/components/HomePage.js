15addc53ecb64a773d38c8e043fe335992b91f65
rebeccay12 <beccayeager97@gmail.com>
2024-01-14 22:20:10 -0600

    homepage added spotify logo

--- src/main/react-app-brainyBunch/src/components/HomePage.js
+++ src/main/react-app-brainyBunch/src/components/HomePage.js
@@ -7,9 +7,14 @@ function HomePage() {
   return (
     <div className="homePage">
       <div>
-        {/* <span className="nav_routes"> */}
         <Navigation />
-        {/* </span> */}
+      </div>
+      {/* Center the Spotify logo */}
+      <div className="centered">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png"
+          alt="Spotify Logo"
+        />
       </div>
     </div>
   );
